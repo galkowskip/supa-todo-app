2fa9388fa3d0499bb900830fbcaef3a997c9b490
galap <galkowski.p97@gmail.com>
2024-05-12 18:46:47 +0200

    connect react to supabase database

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createClient } from '@supabase/supabase-js';
+import React, { useEffect } from 'react';
+
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL as string,
+  process.env.REACT_APP_SUPABASE_KEY as string
+);
+
+interface Todo {
+  id: string;
+  created_at: string;
+  title: string;
+  description: string;
+  isCompleted: boolean;
+  deadline: string;
+}
+
+const getTodos = async () => {
+  const { data, error } = await supabase.from('todos').select('title')
+
+  if (error) {
+    console.error(error);
+    return [] as Todo[];
+  }
+
+  return data as Todo[];
+};
 
 function App() {
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+
+  useEffect(() => {
+    const fetchTodoData = async () => {
+      const newTodos = await getTodos();
+      setTodos(newTodos);
+    };
+
+    fetchTodoData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <pre>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <h2>{todo.title}</h2>
+            <p>{todo.description}</p>
+            <p>{todo.deadline}</p>
+            <p>{todo.isCompleted ? 'Completed' : 'Not Completed'}</p>
+          </div>
+        ))}
+      </pre>
     </div>
   );
 }
